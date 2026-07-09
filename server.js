ce010b1d641b7f785d2082058c66107b43ddbe1a
Komal Revekar <komal.revekar.kit@gmail.com>
2026-07-09 16:05:35 +0530

    Fix all syntax issues

--- server.js
+++ server.js
@@ -14,11 +14,7 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type'],
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
