98c8afb27b95cdc6656c3cdf675ddabdca723473
Komal Revekar <komal.revekar.kit@gmail.com>
2026-07-09 19:35:00 +0530

    Remove debug request logger

--- server.js
+++ server.js
@@ -21,11 +21,7 @@ app.use(cors({
 }));app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Log all incoming requests (for debugging)
-app.use((req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-});
+
 
 // Serve uploaded images as static files
 app.use('/uploads', express.static('uploads'));
